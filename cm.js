2fd2d9976e186f39afe045d353e4d4577232ac40
Snakestongue <maeher.grover@gmail.com>
2026-04-04 10:17:47 -0400

    First Commit

---
+++ cm.js
@@ -0,0 +1,12 @@
+// import {EditorState} from "@codemirror/state" //state of editor
+// import {EditorView, keymap} from "@codemirror/view" //ui
+// import {defaultKeymap} from "@codemirror/commands"//shortcuts, like ctrl c or smth
+
+// let start = EditorState.create({
+//     doc:"hi",
+//     extensions: [keymap.of(defaultKeymap)] // keyboard shortcuts
+// })
+// let view = new EditorView({ //ui component
+//     state:startState, //contains initial 
+//     parent:document.body //parent element of ui
+// })
