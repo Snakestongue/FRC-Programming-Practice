9ee96342d568ae760cb7bf44a8c8f3f2db3740d4
Snakestongue <maeher.grover@gmail.com>
2026-04-10 09:06:04 -0400

    Suggestions Fix 1

--- BACKEND/Suggestions/suggest.js
+++ BACKEND/Suggestions/suggest.js
@@ -1,22 +1,22 @@
 import {suggestion as SuggestModel} from "./suggestSchema.js";
 let createSuggest =async(req, res)=>{
   try{
-    const {suggestReq} = req.body;
-    if (!suggestReq) {
+    const {suggestReq} = req.body
+    if(!suggestReq){
       return res.status(400).json({message: "Field must be filled out"});
     }
     const newSuggest =await SuggestModel.create({
-      suggestions: suggestReq
+      suggest: suggestReq
     });
     res.status(201).json({
       message: "Suggestion posted!",
-      IDSuggest: {id: newSuggest._id},
-    });
+      IDSuggest: {id: newSuggest._id}
+    })
   }catch (error){
     console.log("ERROR", error);
-    res.status(500).json({ message: "Internal server error" +error.message});
+    res.status(500).json({ message: "Internal server error" +error.message})
   }
-};
+}
 export {
   createSuggest
-};
+}
